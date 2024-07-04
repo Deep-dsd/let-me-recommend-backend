f89625a1d669771aab118fed4fce10415b206236
Deep Sankar De <78144390+Deep-dsd@users.noreply.github.com>
2024-07-04 21:01:16 +0530

    first commit

---
+++ src/main/java/com/letMerecommend/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.letMerecommend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
